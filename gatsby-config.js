636f33e2b8eab2c41aead0cb2c8b04691471cecb
Le Quan <leanhquanvn97@gmail.com>
2023-02-01 22:37:46 +0700

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `SaigonDigital Test`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "bHQwRosqLNPM3wY1RsqRWdN1HiJTlo7CGTeXMTA_DJU",
+      "spaceId": "7trptn303oxb"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss"]
+};
